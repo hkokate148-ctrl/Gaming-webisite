3c18bbb6be0278e7ead4a4792b488b8cbb7f7259
Hemant Kokate <hkokate148@gmail.com>
2026-06-28 19:47:34 +0530

    Add files via upload

---
+++ gaming/games.php
@@ -0,0 +1,62 @@
+<?php
+
+$games = [
+
+[
+"name"=>"Test Game",
+"file"=>"https://example.com",
+"img"=>"https://picsum.photos/300/200",
+"rating"=>"⭐⭐⭐⭐",
+"type"=>"free"
+],
+
+[
+"name"=>"2048 Puzzle",
+"file"=>"https://play2048.co/",
+"img"=>"images/2048_1.png",
+"video"=>"games_video/2048.mp4",
+"rating"=>"⭐⭐⭐⭐⭐",
+"type"=>"free"
+],
+
+[
+"name"=>"Snake-Water-Gun",
+"file"=>"snakewatergun.php",
+"img"=>"images/snake1.png",
+"video"=>"games_video/snakewatergun.mp4",
+"rating"=>"⭐⭐⭐⭐☆",
+"type"=>"free"
+],
+
+[
+"name"=>"Tic Tac Toe",
+"file"=>"tictactoe.php",
+"img"=>"images/ttt1.png",
+"video"=>"games_video/tictoctoe.mp4",
+"rating"=>"⭐⭐⭐⭐⭐",
+"type"=>"free"
+],
+
+[
+"name"=>"Snakes & Ladders",
+"file"=>"snakes_ladders.php",
+"img"=>"images/snakes_ladders1.png",
+"video"=>"games_video/snake_ladder.mp4",
+"rating"=>"⭐⭐⭐☆☆",
+"type"=>"free"
+],
+
+[
+"name"=>"Car Racing",
+"file"=>"car_racing.php",
+"img"=>"images/racing1.png",
+"video"=>"games_video/car_racing.mp4",
+"rating"=>"⭐⭐⭐⭐☆",
+"type"=>"free"
+]
+
+
+
+];
+
+?>
